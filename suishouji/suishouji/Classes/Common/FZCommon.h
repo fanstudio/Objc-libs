84af0161dbfb4ed4d9f79da986be83495e8aa3ec
zhangfan <fanstudio@qq.com>
2016-05-14 15:39:26 +0800

    增加说明文件

--- suishouji/suishouji/Classes/Common/FZCommon.h
+++ suishouji/suishouji/Classes/Common/FZCommon.h
@@ -9,5 +9,13 @@
 #ifndef FZCommon_h
 #define FZCommon_h
 
+/** 说明，该文件在实际使用中没有意义，可以删除，放在这里用于方便备注，各类的作用
+ 
+    FZWebViewController - 一个实现了网页浏览功能的的控制器
+    FZDebug             - 用于调试的一些宏定义
+    FZStyle             - APP风格样式的一些宏定义
+    FZMenuController    - 左右分栏结构的APP控制器，类似于知乎日报结构
+    Category            - 通用的一些类扩展
+ */
 
 #endif /* FZCommon_h */
