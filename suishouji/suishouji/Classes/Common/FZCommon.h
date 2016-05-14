dcdc5f45ff4c604944c3bb727639d9e5fb1a3f76
zhangfan <fanstudio@qq.com>
2016-05-14 15:38:15 +0800

    增加说明文件

---
+++ suishouji/suishouji/Classes/Common/FZCommon.h
@@ -0,0 +1,13 @@
+//
+//  FZCommon.h
+//  suishouji
+//
+//  Created by 张帆 on 16/5/13.
+//  Copyright © 2016年 fanstudio. All rights reserved.
+//
+
+#ifndef FZCommon_h
+#define FZCommon_h
+
+
+#endif /* FZCommon_h */
